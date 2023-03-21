6369c6882d2c983a8a9a91673073d085f0dc867c
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-03-21 08:05:36 +0530

    size

--- src/components/App.js
+++ src/components/App.js
@@ -19,19 +19,19 @@ const App = () => {
 
   return (
     <div>
-      <h1>Select your gender</h1>
+      <h2>Select your gender</h2>
       <input type="radio" id="male" name="gender" value="male" checked={selectedGender === 'male'} onChange={handleGenderChange} />
       <label htmlFor="male">Male</label>
       <input type="radio" id="female" name="gender" value="female" checked={selectedGender === 'female'} onChange={handleGenderChange} />
       <label htmlFor="female">Female</label><br/><br/>
-      {what==="male"?(<div><h1>Select your shirt size</h1><br/><br/>
+      {what==="male"?(<div><h2>Select your shirt size</h2><br/><br/>
          <select value={selectedOption} onChange={handleOptionChange}>
         <option value="">Select Size</option>
         <option value="2">Small</option>
         <option value="3">Medium</option>
         <option value="4">Large</option>
       </select>
-      </div>):(<div><h1>Select your dress size</h1><br/><br/>
+      </div>):(<div><h2>Select your dress size</h2><br/><br/>
       <select value={selectedOption} onChange={handleOptionChange}>
         <option value="">Select Size</option>
         <option value="2">2</option>
